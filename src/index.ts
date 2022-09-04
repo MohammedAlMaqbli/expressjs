198e5a68b3e036234239bcaf3b3779fe0b152205
MohammedAlMaqbli <Mohammed.alMaqbli@Gmail.com>
2022-09-04 04:19:46 -0400

    Adding socketio

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,68 @@
-import bodyParser from "body-parser";
 import express from "express";
-
+import { createServer } from "http";
+import { Server } from "socket.io";
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+ });
 const port = process.env.PORT || 3333;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+
 
 app.get("/", async (req, res) => {
   res.json({ Hello: "World" });
 });
 
-app.listen(port, () => {
+
+
+
+
+
+
+
+const users:any = [];
+const members:any = [];
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    socket.on("user_connected", function (user_id) {
+        users[user_id] = socket.id;
+        io.emit("updateUserStatus", users);
+        console.log("user connected " + user_id);
+    });
+
+    socket.on("member_connected", function (user_id) {
+        members[user_id] = socket.id;
+        io.emit("updateMemberStatus", members);
+        console.log("member connected " + user_id);
+    });
+
+    socket.on("disconnect", function () {
+        var i = users.indexOf(socket.id);
+        users.splice(i, 1, 0);
+        io.emit("updateUserStatus", users);
+    });
+
+    socket.on("send_message", function (message) {
+        io.to(`${users[message.receiver_id]}`).emit("receive_message", message);
+    });
+
+    socket.on("send_message-member", function (message) {
+        io.to(`${members[message.receiver_id]}`).emit(
+            "receive_message-member",
+            message
+        );
+    });
+});
+
+httpServer.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+
