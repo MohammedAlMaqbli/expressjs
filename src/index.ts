f8166c8eaf973b26b8e6c16dcdedbc70f3e18e7f
MohammedAlMaqbli <Mohammed.alMaqbli@Gmail.com>
2022-09-04 04:22:48 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createServer } from "http";
+import { createServer } from "https";
 import { Server } from "socket.io";
 const app = express();
 const httpServer = createServer(app);
